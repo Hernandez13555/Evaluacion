af6345a9d62d81be7e2a732ebaa04e5cfdffb140
Hernandez13555 <168384717+Hernandez13555@users.noreply.github.com>
2025-04-04 08:19:05 -0500

    Update database.js

--- utiles/database.js
+++ utiles/database.js
@@ -2,8 +2,8 @@ import mysql from "mysql2/promise";
 
 const conetion = await mysql.createConnection({
   host: "localhost",
-  user: "johan_adso2894667",
-  password: "AprendiT2025",
+  user: "reinel_adso2894667",
+  password: "Aprendiz2025",
   database: "node_adso2894667",
 });
 
